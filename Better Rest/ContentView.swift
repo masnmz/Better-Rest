6f5cde427e7afede69376b19fdabbc9d46ebc62b
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-05-25 21:32:46 +0100

    Date Components

--- Better Rest/ContentView.swift
+++ Better Rest/ContentView.swift
@@ -15,10 +15,21 @@ struct ContentView: View {
             .padding(35)
         DatePicker("PLease Enter a date", selection: $wakeUp, in: Date.now...)
             .labelsHidden()
+            .padding()
+        Text(Date.now.formatted(date: .long, time: .shortened))
     }
     func exampleDates() {
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = Date.now...tomorrow
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//        
+//        let date = Calendar.current.date(from: components) ?? .now
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+        
+        
     }
 }
 
