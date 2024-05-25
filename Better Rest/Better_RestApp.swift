8154a9b92bd82fbd466f3f8de7075abec1e610e8
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-05-25 20:24:33 +0100

    Initial Commit

---
+++ Better Rest/Better_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_RestApp.swift
+//  Better Rest
+//
+//  Created by Mehmet Alp Sönmez on 24/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Better_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Better Rest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Better Rest
+//
+//  Created by Mehmet Alp Sönmez on 24/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
