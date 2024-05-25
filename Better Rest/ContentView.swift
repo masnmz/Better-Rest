fc733bffb8c1ecf51c124fd2ec7ff03b0eb35d0a
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-05-25 20:39:07 +0100

    Date Picker Mock Up

--- Better Rest/ContentView.swift
+++ Better Rest/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
     var body: some View {
-        Stepper("\(sleepAmount) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            .padding(35)
+        DatePicker("PLease Enter a date", selection: $wakeUp, in: Date.now...)
+            .labelsHidden()
+    }
+    func exampleDates() {
+        let tomorrow = Date.now.addingTimeInterval(86400)
+        let range = Date.now...tomorrow
     }
 }
 
